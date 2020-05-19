dc274ff9899baac1b428f8bb99a1bdfdcc29eda5
Adam Podymniak <agpodymniak@gmail.com>
2020-05-19 09:14:08 +0200

    Fixed contact form

--- contactform.php
+++ contactform.php
@@ -1,10 +1,10 @@
 <?php
 
 if (isset($_POST['submit'])) {
-  $name = $_POST['name']
-  $subject = $_POST['subject']
-  $mailFrom = $_POST['mail']
-  $message = $_POST['message']
+  $name = $_POST['name'];
+  $subject = $_POST['subject'];
+  $mailFrom = $_POST['mail'];
+  $message = $_POST['message'];
 
   $mailTo = "agpodymniak@gmail.com";
   $headers = "From: ".$mailFrom;
