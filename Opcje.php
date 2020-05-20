2cde9a4c2ce8b30b6a2a8b885f1416a82c31342d
Adam Podymniak <agpodymniak@gmail.com>
2020-05-20 11:30:39 +0200

    Options

---
+++ Opcje.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="Opcje.css" type="text/css">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Para Do Tańca-Opcje</title>
+</head>
+<body>
+  <div class="jumbotron jumbotron-fluid height100p banner" id="home">
+    <div class="container h100">
+      <div class="contentBox h100">
+        <div>
+          <h1 data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">Wybierz Swoją Ofertę!</h1>
+          <p data-aos="fade-up" data-aos-duration="1000" data-aos-delay="300">Zróżnicowane językowo i kulturowo, wojujące pomiędzy sobą ludy tubylczych Amerykanów, w tym stosunkowo mniej rozszczepieni Inuici, zamieszkiwały obszary dzisiejszych Stanów
+            Zjednoczonych przez około 10 tys. lat.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+  <br><br>
+  <div class="colorbox">
+    <div class="container1">
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>01</h2>
+          <h3>Wybór jeden</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="boxnum1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>02</h2>
+          <h3>Wybór dwa</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+    </div>
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>03</h2>
+          <h3>Wybór trzy</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="colorbox">
+    <div class="container1">
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>01</h2>
+          <h3>Wybór jeden</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>02</h2>
+          <h3>Wybór dwa</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>03</h2>
+          <h3>Wybór trzy</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br><br>
+  <div class="colorbox">
+    <div class="container1">
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>01</h2>
+          <h3>Wybór jeden</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>02</h2>
+          <h3>Wybór dwa</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>03</h2>
+          <h3>Wybór trzy</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+    </div>
+  </div><br><br>
+  <div class="colorbox">
+    <div class="container1">
+      <div class="box1">
+        <div class="content1" data-aos="fade-up" data-aos-duration="1000" data-aos-delay="0">
+          <h2>01</h2>
+          <h3>Wybór jeden</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          <a href="Opcje.php">Więcej</a>
+        </div>
+      </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
